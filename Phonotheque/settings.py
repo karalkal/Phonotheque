7fb70ad054ae6fa9fcdf6bcfe6ac7a3841149b33
karalkal <karalkal@yahoo.co.uk>
2022-04-14 03:41:50 +0100

    fixing debug env

--- Phonotheque/settings.py
+++ Phonotheque/settings.py
@@ -9,8 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'sk')
 
-DEBUG = os.getenv('DEBUG', '')
-
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 APP_ENVIRONMENT = os.getenv('APP_ENVIRONMENT', 'Development')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
