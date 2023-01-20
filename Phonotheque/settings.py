7ed4d210f6dd953895fbc8823726625600366a84
karalkal <karalkal@yahoo.co.uk>
2023-01-20 06:20:08 +0000

    removed .env from github

--- Phonotheque/settings.py
+++ Phonotheque/settings.py
@@ -1,12 +1,12 @@
 import os
 from pathlib import Path
-
-# To read vars from .env
 from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# To read vars from .env
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
